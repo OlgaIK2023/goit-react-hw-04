867945dd70d9924def1ed8036bb47c41a408be88
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-09 16:25:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,21 @@ import ErrorMessage from './components/ErrorMessage/ErrorMessage'
 import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn'
 import ImageModal from './components/ImageModal/ImageModal'
 import ScrollIntoView from 'react-scroll-into-view'
-import ScrollUp from "./ScrollUp/ScrollUp.jsx";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
-function App() {
-
-  const [inputSearch, setInputSearch] = useState("")
-  const [photos, setPhotos] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [page, setPage] = useState(false)
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [inputSearch, setInputSearch] = useState("");
+  const [page, setPage] = useState(1);
   const [showBtn, setShowBtn] = useState(false);
   const [imageSrc, setImageSrc] = useState(null);
   const [description, setDescription] = useState(null);
-  const lastImageRef = useRef(null);
   const [scrollBtn, setScrollBtn] = useState(false);
+  const lastImageRef = useRef(null);
 
-
-  useEffect(() => {
+useEffect(() => {
     if (!inputSearch) return;
     async function fetchPhotos()  {
     try {
@@ -89,6 +87,6 @@ const scrollToLastImage = () => {
     
     </>
   );
-}
+};
 
 export default App
